751974ac5caf4caf89f6096bcd3acfbf58518ed2
Vinicius Dalaqua <viniciushdalaqua@gmail.com>
2024-11-12 09:07:05 -0300

    projeto criado

---
+++ src/main/java/com/viniciusdalaqua/GateControl/GateControlApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusdalaqua.GateControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusdalaqua/GateControl/GateControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusdalaqua.GateControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
