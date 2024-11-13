fae5eea75f4a449a4c62d4bf2c633b29b10d4684
Vinicius Dalaqua <viniciushdalaqua@gmail.com>
2024-11-13 02:51:13 -0300

    métodos CRUD e RestController da entidade Vehicle criados

--- src/main/java/com/viniciusdalaqua/GateControl/resources/VehicleResource.java
+++ src/main/java/com/viniciusdalaqua/GateControl/resources/VehicleResource.java
@@ -3,9 +3,7 @@ package com.viniciusdalaqua.GateControl.resources;
 import com.viniciusdalaqua.GateControl.entities.Vehicle;
 import com.viniciusdalaqua.GateControl.services.VehicleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,30 @@ public class VehicleResource {
     public ResponseEntity<List<Vehicle>> findAll(){
         List<Vehicle> vehicles = vehicleService.findAll();
         return ResponseEntity.ok().body(vehicles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Vehicle> findById(@PathVariable Long id){
+        Vehicle obj = vehicleService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Vehicle> insert( @RequestBody Vehicle vehicle){
+        Vehicle vehicleInserted = vehicleService.insert(vehicle);
+        return ResponseEntity.ok().body(vehicleInserted);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Vehicle> update(@PathVariable long id, @RequestBody Vehicle obj){
+        Vehicle vehicleUpdated = vehicleService.update(id, obj);
+        return ResponseEntity.ok().body(vehicleUpdated);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        vehicleService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/viniciusdalaqua/GateControl/services/VehicleService.java
+++ src/main/java/com/viniciusdalaqua/GateControl/services/VehicleService.java
@@ -5,6 +5,7 @@ import com.viniciusdalaqua.GateControl.repositories.VehicleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleService {
@@ -17,5 +18,27 @@ public class VehicleService {
 
     public List<Vehicle> findAll() {
         return vehicleRepository.findAll();
+    }
+
+    public Vehicle findById(Long id) {
+        Optional<Vehicle> vehicle = vehicleRepository.findById(id);
+        return vehicle.orElseThrow();
+    }
+    public Vehicle insert(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+    public Vehicle update(Long id, Vehicle obj) {
+        Vehicle entity = vehicleRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return vehicleRepository.save(entity);
+    }
+    public void updateData(Vehicle entity, Vehicle obj) {
+        entity.setPlate(obj.getPlate());
+        entity.setModel(obj.getModel());
+        entity.setColor(obj.getColor());
+        entity.setOwner(obj.getOwner());
+    }
+    public void delete(Long id){
+        vehicleRepository.deleteById(id);
     }
 }
